170cd51f1ddfc979467bf8bfccb99a62d68aa2da
Kyle Anne <kyle.laymon@gmail.com>
2020-05-13 19:29:42 -0700

    initial decent version

--- pages/index.js
+++ pages/index.js
@@ -1,207 +1,285 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
+import TwitchJs from 'twitch-js'
 
-export default function Home() {
+const enigmaPurple = '#4700ff'
+const anEternalEnigma = { userId: '20485198', username: 'aneternalenigma' }
+const ignoredUsers = { 'aneternalenigma': true, 'aneternalbot': true }
+let recordedChatters = {}
+let isRecording = false
+
+// start initialize Twitch Clients
+const { chat } = new TwitchJs({ log: { level: 'silent' }})
+const { api } = new TwitchJs({
+  log: { level: 'error' },
+  clientId: '',
+  token: ''
+})
+
+// end initialize Twitch Clients
+
+export default function Home () {
+  // start state hooks
+  const [ currentlyDisplaying, setIsCurrentlyDisplaying ] = useState('allChatters')
+  const [ chooseWinnerFrom, setChooseWinnerFrom ] = useState('nonSubsOnly')
+  const [ isRecordingState, setIsRecordingState ] = useState(isRecording)
+  const [ isSelectingWinner, setIsSelectingWinner ] = useState(false)
+
+  const [ recordedChattersState, setRecordedChatters ] = useState({})
+  const [ subbedChatters, setSubbedChatters ] = useState({})
+  const [ unsubbedChatters, setUnsubbedChatters ] = useState({})
+  const [ winner, setWinner ] = useState({})
+  // end state hooks
+
+  // start handle on mount with useEffect hook
+  useEffect(() => {
+    chat.removeAllListeners()
+    chat.on(TwitchJs.Chat.Events.PARSE_ERROR_ENCOUNTERED, () => {})
+    chat.connect().then(() => chat.join(anEternalEnigma.username))
+
+    chat.on(TwitchJs.Chat.Events.PRIVATE_MESSAGE, ({ username }) => {
+      console.log(1, recordedChatters)
+      // STATE IS FUCKY HERE ??
+      if (isRecording && !recordedChatters[username] && !ignoredUsers[username] ) {
+        recordedChatters[username] = { username }
+        console.log(2, recordedChatters)
+
+        api.get('users', { search: { login: username } })
+        .then(({ data }) => {
+          const userId = data.length && data[0].id
+          if (!userId) return
+
+          api.get('subscriptions', { search: { broadcaster_id: anEternalEnigma.userId, user_id: userId } })
+          .then(({ data }) => {
+            const isUserSubbed = !!data[0]
+            const newRecordedChatters = {...recordedChatters}
+            newRecordedChatters[username] = { isUserSubbed, userId, username }
+            recordedChatters = newRecordedChatters
+            setRecordedChatters(prevObject => ({...prevObject, ...newRecordedChatters}))
+
+            if (isUserSubbed) {
+              updateSubbedChatters({ isUserSubbed, userId, username })
+            } else {
+              const newUnsubbedChatters = {...unsubbedChatters}
+              newUnsubbedChatters[username] = { isUserSubbed, userId, username }
+              setUnsubbedChatters(prevObject => ({...prevObject, ...newUnsubbedChatters}))
+            }
+          })
+        })
+      }
+    })
+  }, [])
+  // end handle on mount with useEffect hook
+
+  const clearChatters = () => {
+    setRecordedChatters({})
+    setSubbedChatters({})
+    setUnsubbedChatters({})
+    setWinner({})
+  }
+
+  const toggleIsRecording = () => {
+    isRecording = !isRecording
+    setIsRecordingState(!isRecordingState)
+  }
+
+  const updateSubbedChatters = ({ isUserSubbed, userId, username }) => {
+    const newSubbedChatters = {...subbedChatters}
+    newSubbedChatters[username] = { isUserSubbed, userId, username }
+    setSubbedChatters(prevObject => ({...prevObject, ...newSubbedChatters}))
+
+    if (unsubbedChatters[username]) {
+      const newUnsubbedChatters = {...unsubbedChatters}
+      delete newUnsubbedChatters[potentialWinner.username]
+      setUnsubbedChatters(newUnsubbedChatters)
+    }
+  }
+
+  const updateChatterToSub = (data) => {
+    console.log('updating', data)
+    const newSubbedChatters = {...subbedChatters}
+    newSubbedChatters[username] = { isUserSubbed, userId, username }
+    setSubbedChatters(prevObject => ({...prevObject, ...newSubbedChatters}))
+  }
+
+  const handleDisplayChange = (e) => {
+    setIsCurrentlyDisplaying(e.target.value)
+  }
+
+  const handleWinnerChange = (e) => {
+    setChooseWinnerFrom(e.target.value)
+  }
+
+  const getWinner = () => {
+    setWinner({})
+    setIsSelectingWinner(true)
+    const chatterObjects = {
+      'allChatters': recordedChatters,
+      'nonSubsOnly': unsubbedChatters,
+      'subsOnly': subbedChatters
+    }
+    const selectedChatterObject = chatterObjects[chooseWinnerFrom]
+    const numberOfChatters = Object.keys(selectedChatterObject).length
+
+    if (!numberOfChatters) return setIsSelectingWinner(false)
+    if (numberOfChatters < 2) {
+      setIsSelectingWinner(false)
+      return setWinner(Object.values(selectedChatterObject)[0])
+    }
+
+    fetch(`https://www.random.org/integers/?num=1&min=0&max=${numberOfChatters - 1}&col=1&base=10&format=plain`,
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.text())
+      .then(data => {
+        const potentialWinner = Object.values(selectedChatterObject)[parseInt(data)]
+
+        if (chooseWinnerFrom !== 'nonSubsOnly') {
+          setIsSelectingWinner(false)
+          return setWinner(potentialWinner)
+        }
+
+        // if we're gifting to non-subs only double check they already haven't been gifted one tonight
+        api.get('subscriptions', { search: { broadcaster_id: anEternalEnigma.userId, user_id: potentialWinner.userId } })
+          .then(({ data }) => {
+            const isUserSubbed = !!data[0]
+            if (isUserSubbed) {
+              updateSubbedChatters({ isUserSubbed, userId: potentialWinner.userId, username: potentialWinner.username})
+              return getWinner()
+            } else {
+              setIsSelectingWinner(false)
+              return setWinner(potentialWinner)
+            }
+          })
+      })
+  }
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className='container'>
+      <Head> <title>Active Chatter List</title></Head>
+      <h1> Choose Winner From: </h1>
+      <div className='row' >
+        <input onChange={handleWinnerChange} type='radio' name='filterPotentialWinner' id='allChattersWinner' value='allChatters' checked={chooseWinnerFrom === 'allChatters'} />
+        <label htmlFor='allChattersWinner'>All Chatters</label>
+        <input onChange={handleWinnerChange} type='radio' name='filterPotentialWinner' id='subsOnlyWinner' value='subsOnly' checked={chooseWinnerFrom === 'subsOnly'} />
+        <label htmlFor='subsOnlyWinner'>Subs Only</label>
+        <input onChange={handleWinnerChange} type='radio' name='filterPotentialWinner' id='nonSubsOnlyWinner' value='nonSubsOnly' checked={chooseWinnerFrom === 'nonSubsOnly'} />
+        <label htmlFor='nonSubsOnlyWinner'>Non-Subs Only</label>
+      </div>
+      {!!winner.username &&
+      <div>
+        <h1>AND THE WINNER IS</h1>
+        <h2 className='accentColor'>{winner.username}</h2>
+      </div>
+      }
+      <button onClick={getWinner}>{isSelectingWinner ? 'CHOOSING...' : 'CHOOSE WINNER'} </button>
+      <h1> Display list of: </h1>
+      <div className='row'>
+        <input onChange={handleDisplayChange} type='radio' name='filterChatterType' id='allChatters' value='allChatters' checked={currentlyDisplaying === 'allChatters'} />
+        <label htmlFor='allChatters'>All Chatters</label>
+        <input onChange={handleDisplayChange} type='radio' name='filterChatterType' id='subsOnly' value='subsOnly' checked={currentlyDisplaying === 'subsOnly'} />
+        <label htmlFor='subsOnly'>Subs Only</label>
+        <input onChange={handleDisplayChange} type='radio' name='filterChatterType' id='nonSubsOnly' value='nonSubsOnly' checked={currentlyDisplaying === 'nonSubsOnly'} />
+        <label htmlFor='nonSubsOnly'>Non-Subs Only</label>
+      </div>
+      <div className='buttonRow'>
+        <button onClick={toggleIsRecording}>{isRecordingState ? 'STOP' : 'START'} RECORDING</button>
+        <button onClick={clearChatters}>CLEAR CHATTERS</button>
+      </div>
+      {currentlyDisplaying === 'allChatters' &&
+        <div>
+          {Object.values(recordedChatters).map((chatter, index) =>
+            <div className='row' key={index}>
+              <div>{chatter.username}</div>
+              <div className={!!chatter.isUserSubbed ? 'accentColor' : ''}>{!!chatter.isUserSubbed ? 'Subbed!' : 'Not Subbed!'}</div>
+            </div>
+          )}
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
+      }
+      {currentlyDisplaying === 'subsOnly' &&
+        <div>
+          {Object.values(subbedChatters).map((chatter, index) =>
+            <div className='row' key={index}>
+              <div>{chatter.username}</div>
+              <div>{!!chatter.isUserSubbed && 'Is Subbed!'}</div>
+            </div>
+          )}
+        </div>
+      }
+      {currentlyDisplaying === 'nonSubsOnly' &&
+        <div>
+          {Object.values(unsubbedChatters).map((chatter, index) =>
+            <div className='row' key={index}>
+              <div>{chatter.username}</div>
+              <div>{!!chatter.isUserSubbed && 'Is Subbed!'}</div>
+            </div>
+          )}
+        </div>
+      }
       <style jsx>{`
+        .accentColor {
+          color: ${enigmaPurple};
+        }
+
         .container {
+          align-items: center;
+          display: flex;
+          flex-direction: column;
+          font-size: 1.4rem;
           min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
           text-align: center;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
+        .buttonRow {
           display: flex;
-          align-items: center;
           justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
+          width: 40rem;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .row {
+          display: flex;
+          justify-content: space-between;
+          width: 40rem;
         }
       `}</style>
-
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
+        body, html {
+          background: black;
+          color: white;
+          font-size: 10px;
           margin: 0;
+          padding: 0.5rem;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+          Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
+        button {
+          background-color: ${enigmaPurple};
+          border: none;
+          color: black;
+          cursor: pointer;
+          font-size: 1.2rem;
+          font-weight: 600;
+          margin: 3rem 1rem;
+          padding: 1rem;
+        }
+        input {
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
+          border: 0.1rem solid white;
+          border-radius: 50%;
+          cursor: pointer;
+          height: 1.5rem;
+          width: 1.5rem;
 
-        * {
-          box-sizing: border-box;
+        }
+        input:checked {
+          border: 0.5rem solid ${enigmaPurple};
         }
       `}</style>
     </div>
