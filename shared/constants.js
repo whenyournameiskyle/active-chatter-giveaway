789ef33d71eb69a84481be730eae07c998bb33d3
Kyle Anne Laymon <kyle.laymon@gmail.com>
2021-11-30 14:12:50 -0800

    chore: add more popular chatbots to ignoredUsers

--- shared/constants.js
+++ shared/constants.js
@@ -2,7 +2,10 @@ export const ignoredUsers = {
   ananonymouscheerer: 1,
   ananonymousgifter: 1,
   streamelements: 1,
+  streamelements: 1,
+  streamlabs: 1,
   moobot: 1,
-  nightbot: 1
+  nightbot: 1,
+  pretzelrocks: 1,
 }
 export const isDevelopment = process.env.NODE_ENV === 'development'
